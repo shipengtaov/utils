770bef4c237b5cf0ec79985f8b25d6eee1315e4e
shispt <shispt18@gmail.com>
2014-10-23 14:30:21 +0800

    add shutdown_funciton

--- functions.php
+++ functions.php
@@ -23,13 +23,34 @@ function array_htmlspecialchars($arr){
 	return $arr;
 }
 
-function time_count_wrapper(callable $fn, $params){
+function time_count_wrapper(callable $fn, $params = array(), $print_direct = True){
+	global $print_call_info;
+
+	// print_r($print_call_info);
+
 	if(!is_callable($fn)){
 		exit($fn . ' is not callable');
 	}
 	$start = explode(' ', microtime());
 	$result = call_user_func_array($fn, $params);
 	$end = explode(' ', microtime());
-	echo '<div style="background:#808080;padding:2px;position:relative;left:20px;"><code>', $fn, '() 耗时: ', ( ($end[0]+$end[1]) - ($start[0]+$start[1]) ), 's</code></div>';
+	$msg = '<div style="background:#808080;padding:2px;margin:2px;position:relative;left:20px;">';
+	$msg .= '<code>';
+	$msg .= $fn . '() 耗时: ' . ( ($end[0]+$end[1]) - ($start[0]+$start[1]) ) . 's';
+	$msg .= '</code>';
+	$msg .= '</div>';
+	if($print_direct){
+		echo $msg;
+	}
+	$print_call_info .= $msg;
 	return $result;
+}
+
+if($debug == 1){
+	register_shutdown_function('print_call_info');
+}
+
+function print_call_info(){
+	global $print_call_info;
+	echo $print_call_info;
 }
